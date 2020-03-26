deb316d4e5cc28d4b6c036f27bf9b7c0bc7e76a0
Nacho <ignacioabadie@live.com.ar>
2020-03-25 22:17:31 -0300

    Kickoff

---
+++ Script.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Acá se declaran las variables del script
+# var a = 2
+# var b = "text"
+
+
+# esta función solo se llama una vez, al iniciar la instancia
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Esta función se llama en cada Frame
+func _process(delta):
+	
+	pass
